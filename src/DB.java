6583f4fde5931530a62f1e6248a6d0f998d19a6c
Enzo <enzo.allemano@hotmail.com>
2021-01-08 14:58:15 +0100

    Ajout du schéma par défaut "projet"

--- src/DB.java
+++ src/DB.java
@@ -4,19 +4,19 @@ public class DB {
 
     public static void main(String ... args) throws SQLException {
 
-        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bdr", "bdr", "bdr");
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bdr?currentSchema=projet", "bdr", "bdr");
 
         System.out.println("Java JDBC PostgreSQL Example");
         // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
         // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
-//          Class.forName("org.postgresql.Driver");
+        //Class.forName("org.postgresql.Driver");
 
         System.out.println("Connected to PostgreSQL database!");
         Statement statement = connection.createStatement();
         System.out.println("Reading car records...");
 
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM pagila.actor");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM utilisateur");
         ResultSetMetaData rsmd = resultSet.getMetaData();
         int columnsNumber = rsmd.getColumnCount();
         while (resultSet.next()) {
