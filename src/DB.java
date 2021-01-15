4246f200ec7afd9866254017151f15b89e7114bb
Yohann <yohannx11@hotmail.com>
2021-01-15 15:45:26 +0100

    test

--- src/DB.java
+++ src/DB.java
@@ -78,7 +78,6 @@ public class DB implements IDBAccess{
     }
 
 
-     // non
     @Override
     public ResultSet getDrink(String name) throws SQLException {
         preparedStatement = connection.prepareStatement("SELECT * FROM boisson_alcolise WHERE nom =?");
@@ -114,8 +113,6 @@ public class DB implements IDBAccess{
                 System.out.print(columnValue + " " + rsmd.getColumnName(i) +",\n");
             }
         }
-
-
         return null;
     }
 
